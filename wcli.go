d6254b7d9af961bef28391937c14961da2ba1e9e
James Beasley <jabeasley2018@proton.me>
2025-05-08 20:11:06 -0500

    added json structs, print current weather and todays summary

--- wcli.go
+++ wcli.go
@@ -1,23 +1,94 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"flag"
-	"fmt"
 )
 
-func main(){
-	
-	lat := flag.String("lat","","latitude")
-	lon := flag.String("lon","","longitude")
-	appid := flag.String("appid","","api key from Open Weather Map")
+type weatherResponse struct {
+	Lat            float64 `json:"lat"`
+	Lon            float64 `json:"lon"`
+	Timezone       string  `json:"timezone"`
+	TimezoneOffset int     `json:"timezone_offset"`
+
+	Current struct {
+		Dt         int     `json:"dt"`
+		Sunrise    int     `json:"sunrise"`
+		Sunset     int     `json:"sunset"`
+		Temp       float64 `json:"temp"`
+		FeelsLike  float64 `json:"feels_like"`
+		Pressure   int     `json:"pressure"`
+		Humidity   int     `json:"humidity"`
+		DewPoint   float64 `json:"dew_point"`
+		Uvi        float64 `json:"uvi"`
+		Clouds     int     `json:"clouds"`
+		Visibility int     `json:"visibility"`
+		WindSpeed  float64 `json:"wind_speed"`
+		WindDeg    int     `json:"wind_deg"`
+		WindGust   float64 `json:"wind_gust"`
+		Weather    []struct {
+			ID          int    `json:"id"`
+			Main        string `json:"main"`
+			Description string `json:"description"`
+			Icon        string `json:"icon"`
+		} `json:"weather"`
+	} `json:"current"`
+
+	Daily []struct {
+		Dt        int     `json:"dt"`
+		Sunrise   int     `json:"sunrise"`
+		Sunset    int     `json:"sunset"`
+		Moonrise  int     `json:"moonrise"`
+		Moonset   int     `json:"moonset"`
+		MoonPhase float64 `json:"moon_phase"`
+		Summary   string  `json:"summary"`
+		Temp      struct {
+			Day   float64 `json:"day"`
+			Min   float64 `json:"min"`
+			Max   float64 `json:"max"`
+			Night float64 `json:"night"`
+			Eve   float64 `json:"eve"`
+			Morn  float64 `json:"morn"`
+		} `json:"temp"`
+		FeelsLike struct {
+			Day   float64 `json:"day"`
+			Night float64 `json:"night"`
+			Eve   float64 `json:"eve"`
+			Morn  float64 `json:"morn"`
+		} `json:"feels_like"`
+		Pressure  int     `json:"pressure"`
+		Humidity  int     `json:"humidity"`
+		DewPoint  float64 `json:"dew_point"`
+		WindSpeed float64 `json:"wind_speed"`
+		WindDeg   int     `json:"wind_deg"`
+		WindGust  float64 `json:"wind_gust"`
+		Weather   []struct {
+			ID          int    `json:"id"`
+			Main        string `json:"main"`
+			Description string `json:"description"`
+			Icon        string `json:"icon"`
+		} `json:"weather"`
+		Clouds int     `json:"clouds"`
+		Pop    float64 `json:"pop"`
+		Rain   float64 `json:"rain,omitempty"`
+		Uvi    float64 `json:"uvi"`
+	} `json:"daily"`
+}
+
+func main() {
+
+	lat := flag.String("lat", "", "latitude")
+	lon := flag.String("lon", "", "longitude")
+	appid := flag.String("appid", "", "api key from Open Weather Map")
 	flag.Parse()
 
-	url := fmt.Sprintf("https://api.openweathermap.org/data/3.0/onecall?lat=%s&lon=%s&appid=%s", *lat, *lon, *appid)
-	
-	resp,err := http.Get(url)
+	url := fmt.Sprintf("https://api.openweathermap.org/data/3.0/onecall?lat=%s&lon=%s&units=imperial&exclude=minutely,hourly,alerts&appid=%s", *lat, *lon, *appid)
+
+	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -28,6 +99,12 @@ func main(){
 		log.Fatalln(err)
 	}
 
-	sb := string(body)
-	log.Printf(sb)
+	var weatherData weatherResponse
+	err = json.Unmarshal(body, &weatherData)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("Current Temp: %.1f°C\n", weatherData.Current.Temp)
+	fmt.Printf("Weather: %s\n", weatherData.Daily[0].Summary)
 }
